91a1e4d08f24863b3e369b6612f158836cff7c22
williezh <276971837@qq.com>
2017-04-06 20:27:36 -0400

    modify createdatabase.py

--- createdatabase.py
+++ createdatabase.py
@@ -1,10 +1,14 @@
 #coding:utf-8
-import os
-import time
 import pymysql
-from kcsj.local_settings import DATABASES 
 
-dbconf=DATABASES.get('default')
+from importlib import import_module
+#search the dirname of local_settings.py and import it
+with open('manage.py') as f:
+    s=f.read()
+d=re.search(r'DJANGO_SETTINGS_MODULE.*?,\s*"(.+?)settings',s).group(1)
+mo=import_module(d+'local_settings')
+
+dbconf=mo.DATABASES.get('default')
 config={'host':dbconf.get('HOST'),
     'user':dbconf.get('USER'),
     'passwd':dbconf.get('PASSWORD'),
